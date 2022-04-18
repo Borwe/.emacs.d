f2c50d9e8ef52935802535a2f58ef697ef32a307
brian.orwe <brian.orwe@gmail.com>
2022-04-18 16:08:52 +0300

    Create init.el

---
+++ init.el
@@ -0,0 +1,197 @@
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1)   ; Disable the toolbar
+(tooltip-mode -1)    ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing room
+
+(menu-bar-mode -1) ; Disable the menu bar
+(setq visible-bell t) ; Setup the visible bell
+
+(column-number-mode)
+
+(global-display-line-numbers-mode t)
+(dolist (mode '(org-mode-hook term-mode-hook shell-mode-hook eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; set font
+(set-face-attribute 'default nil :font "Noto Mono" :height 100) 
+
+
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;;			 ("melpa-stable" . "https://stable.melpa.org/packages")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+;;Initialize use-package on non-linux plaforms
+;(unless (package-installed-p 'use-package) (package-install 'use-pacakge))
+(package-install 'use-package)
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(custom-safe-themes
+   (quote
+    ("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default)))
+ '(exwm-floating-border-color "#242530")
+ '(fci-rule-color "#6272a4")
+ '(global-command-log-mode t)
+ '(highlight-tail-colors
+   ((("#2c3e3c" "#2a3b2e" "green")
+     . 0)
+    (("#313d49" "#2f3a3b" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1E2029" "#bd93f9"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1E2029" "#50fa7b"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1E2029" "#565761"))
+ '(objed-cursor-color "#ff5555")
+ '(package-selected-packages
+   (quote
+    (evil-magit magit counsel-projectile projectile wakatime-mode evil-collection evil general geenral doom-themes helpful ivy-rich counsel which-key rainbow-delimiters doom-modeline swiper ivy command-log-mode use-package use-pacakge cmake-mode)))
+ '(pdf-view-midnight-colors (cons "#f8f8f2" "#282a36"))
+ '(rustic-ansi-faces
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(vc-annotate-background "#282a36")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50fa7b")
+    (cons 40 "#85fa80")
+    (cons 60 "#bbf986")
+    (cons 80 "#f1fa8c")
+    (cons 100 "#f5e381")
+    (cons 120 "#face76")
+    (cons 140 "#ffb86c")
+    (cons 160 "#ffa38a")
+    (cons 180 "#ff8ea8")
+    (cons 200 "#ff79c6")
+    (cons 220 "#ff6da0")
+    (cons 240 "#ff617a")
+    (cons 260 "#ff5555")
+    (cons 280 "#d45558")
+    (cons 300 "#aa565a")
+    (cons 320 "#80565d")
+    (cons 340 "#6272a4")
+    (cons 360 "#6272a4")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package command-log-mode)
+
+(use-package swiper)
+
+(use-package wakatime-mode)
+(global-wakatime-mode)
+
+(use-package ivy
+	     :diminish
+	     :bind (("C-s" . swiper)
+		    :map ivy-minibuffer-map
+		    ("TAB" . ivy-alt-done)
+		    ("C-l" . ivy-alt-done)
+		    ("C-j" . ivy-next-line)
+		    ("C-k" . ivy-previous-line)
+		    :map ivy-switch-buffer-map
+		    ("C-k" . ivy-previous-line)
+		    ("C-l" . ivy-done)
+		    ("C-d" . ivy-switch-buffer-kill)
+		    :map ivy-reverse-i-search-map
+		    ("C-k" . ivy-previous-line)
+		    ("C-d" . ivy-reverse-i-search-kill))
+	     :config
+	     (ivy-mode 1))
+
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 3)))
+
+(use-package doom-themes)
+(load-theme 'doom-dracula t)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config (setq ivy-initial-inputs-alist nil))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable0-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package general)
+(general-define-key
+ "C-M-j" 'counsel-switch-buffer)
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  ;;:hook (evil-mode . rune/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config (evil-collection-init))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap ("C-c p" . projectile-command-map))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit)
+
+(use-package evil-magit
+  :after magit)
